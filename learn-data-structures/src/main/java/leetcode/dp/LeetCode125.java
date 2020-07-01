e7f1f17e5f5cdc331b1bcb38a61cc756752df07c
yeguxin <taozhengzhi@data88.cn>
2020-07-01 18:38:07 +0800

    LeetCode 100. 相同的树

---
+++ learn-data-structures/src/main/java/leetcode/dp/LeetCode125.java
@@ -0,0 +1,18 @@
+package leetcode.dp;
+
+/**
+ * @author: yeguxin
+ * @date: 2020/7/1
+ * @description:
+ */
+public class LeetCode125 {
+    public boolean isPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        
+        return false;
+    }
+    
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ learn-data-structures/src/main/java/leetcode/model/TreeNode.java
@@ -0,0 +1,16 @@
+package leetcode.model;
+
+/**
+ * @author: yeguxin
+ * @date: 2020/7/1
+ * @description:
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    TreeNode(int x) {
+        val = x;
+    }
+}
