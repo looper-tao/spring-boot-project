4ce839179284dd6cbaecc137a121283793daafc9
yeguxin <taozhengzhi@data88.cn>
2020-07-01 18:16:43 +0800

    LeetCode 53. 最大子序和

---
+++ learn-data-structures/src/main/java/leetcode/dp/LeetCode53.java
@@ -0,0 +1,50 @@
+package leetcode.dp;
+
+/**
+ * @author: yeguxin
+ * @date: 2020/7/1
+ * @description:
+ *
+ * 53. 最大子序和
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ *
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ *
+ */
+public class LeetCode53 {
+    
+    public int maxSubArray(int[] nums) {
+        
+        int[] dp = new int[nums.length];
+        
+        int len = nums.length;
+        dp[0] = nums[0];
+        int max = dp[0];
+        for(int i=1;i<len;i++){
+            dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
+            if(dp[i] > max){
+                max = dp[i];
+            }
+        }
+        
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        
+        LeetCode53 leetCode53 = new LeetCode53();
+    
+        int maxSubArray = leetCode53.maxSubArray(nums);
+        
+        System.out.println(maxSubArray);
+    }
+}
