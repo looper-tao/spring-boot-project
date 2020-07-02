085bcd314eb31ccb7c90907073c95278cbe2d020
yeguxin <taozhengzhi@data88.cn>
2020-07-02 14:12:05 +0800

    类初始化顺序整理

---
+++ learn-java-core/src/main/java/com/learn/initialize/Father.java
@@ -0,0 +1,33 @@
+package com.learn.initialize;
+
+/**
+ * @author: yeguxin
+ * @date: 2020/7/2
+ * @description:
+ */
+public class Father {
+    private int i = test();
+    private static int j = method();
+    
+    static {
+        System.out.print("(1)");
+    }
+    
+    Father() {
+        System.out.print("(2)");
+    }
+    
+    {
+        System.out.print("(3)");
+    }
+    
+    public int test() {
+        System.out.print("(4)");
+        return 1;
+    }
+    
+    public static int method() {
+        System.out.print("(5)");
+        return 1;
+    }
+}
---
+++ learn-java-core/src/main/java/com/learn/initialize/Son.java
@@ -0,0 +1,40 @@
+package com.learn.initialize;
+
+/**
+ * @author: yeguxin
+ * @date: 2020/7/2
+ * @description:
+ */
+public class Son extends Father {
+    private int i = test();
+    private static int j = method();
+    
+    static {
+        System.out.print("(6)");
+    }
+    
+    Son() {
+        System.out.print("(7)");
+    }
+    
+    {
+        System.out.print("(8)");
+    }
+    
+    @Override
+    public int test() {
+        System.out.print("(9)");
+        return 1;
+    }
+    
+    public static int method() {
+        System.out.print("(10)");
+        return 1;
+    }
+    
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        System.out.println();
+        Son s2 = new Son();
+    }
+}
